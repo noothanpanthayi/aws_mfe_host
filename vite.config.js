6eb3f0666b5d38d19657132cc02a00d898a4fb25
noothanpanthayi <noothankrishnan@outlook.com>
2024-12-28 16:37:30 -0600

    update2

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,8 @@ export default defineConfig({
     federation({
       name:"app",
       remotes:{
-        mfeApp1:"http://localhost:6001/child1/assets/remoteEntry.js",
-        mfeApp2:"http://localhost:6002/child2/assets/remoteEntry.js",
+        mfeApp1:"http://localhost:6001/page1/assets/remoteEntry.js",
+        mfeApp2:"http://localhost:6002/page2/assets/remoteEntry.js",
       },
       shared:["react", "react-dom"]
      })

dbeea0c0c163e7a025653a20cc3adf2b8dc938f9
noothanpanthayi <noothankrishnan@outlook.com>
2024-12-28 21:17:07 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import federation from '@originjs/vite-plugin-federation'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     federation({
-      name:"app",
-      remotes:{
-        mfeApp1:"http://localhost:6001/page1/assets/remoteEntry.js",
-        mfeApp2:"http://localhost:6002/page2/assets/remoteEntry.js",
+      name: "app",
+      remotes: {
+        mfeApp1: "http://localhost:6001/page1/assets/remoteEntry.js",
+        mfeApp2: "http://localhost:6002/page2/assets/remoteEntry.js",
       },
-      shared:["react", "react-dom"]
-     })
+      shared: ["react", "react-dom"],
+    }),
   ],
-  build:{
-    modulePreload:false,
-    target:"esnext",
-    minify:false,
-    cssCodeSplit:false
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
   },
-  server: { 
-    cors: { 
-    port:'4173',
-    origin: '*',  
-    methods: ['GET', 'POST', 'OPTIONS'],  
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-     }
-    }
-  });
+  server: {
+    cors: {
+      port: "4173",
+      origin: "*",
+      methods: ["GET", "POST", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    },
+    proxy: {
+      "/page1": {
+        target: "http://localhost:6001",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/page2": {
+        target: "http://localhost:6002",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
